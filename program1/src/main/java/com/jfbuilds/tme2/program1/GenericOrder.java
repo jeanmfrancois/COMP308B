417fa8ebcc7743ca5086df9927cdf64d45116536
Jean-francois <jeanmfrancois@hotmail.com>
2014-10-04 16:59:30 -0400

    Create GenericOrder.java, Backup Preferences Inside admin Directory

---
+++ program1/src/main/java/com/jfbuilds/tme2/program1/GenericOrder.java
@@ -0,0 +1,27 @@
+/**
+ * File Name: GenericOrder.java<br>
+ * Jean-francois Nepton<br>
+ * COMP 308 Java for Programmers<br>
+ * Cordinator: Dr. Xiaokun Zhang<br>
+ * Student ID# 2358976<br>
+ * Assignment: TME 2<br>
+ * Created: Sep 30, 2014
+ */
+package com.jfbuilds.tme2.program1;
+
+/**
+ * A generic container that acts as a collection of an arbitrary number of
+ * objects of type Products or a subclass.
+ * <p>
+ * Field id gives each item in the collection a unique number.
+ * <p>
+ * Methods exist for common actions for a collection as well as an override of
+ * the toString method to offer a readable representation of the item in the
+ * collection.
+ * 
+ * @author Jean-francois Nepton
+ * @version %I%, %G%
+ * @since 1.0
+ */
+public class GenericOrder<Product> {
+}
